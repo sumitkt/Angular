90b61d61debda2a10074aad0c96f08affff92b4c
sumit <sumitkrthakur18@gmail.com>
2021-09-06 15:08:45 +0530

    functionality added in servers.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,24 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]',
   //selector: '.app-servers',
   selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+  //template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl:'./servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer=false;
+  serverStatus='No server was created';
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer=true;
+    }, 4000);
+  }
 
   ngOnInit(): void {
+  }
+
+  getServerStatus(){
+    this.serverStatus='A server was created';
   }
 
 }
